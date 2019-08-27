4820e9005ceebb61ad013da0bbb20db14bb58486
Reza Enayati <enayatii.reza@gmail.com>
2019-08-27 22:59:14 +0430

    add whole league logoes

--- utils/getLogo.js
+++ utils/getLogo.js
@@ -1,5 +1,19 @@
 const logos = {
   Nassaji: require('../assets/images/icon.png'),
   'Gol Gohar': require('../assets/images/logo/Golgoharfclogo.png'),
+  Peykan: require('../assets/images/logo/PeykanLogo.png'),
+  Sepahan: require('../assets/images/logo/Sepahan.png'),
+  Persepolis: require('../assets/images/logo/Persepolis.png'),
+  Esteghlal: require('../assets/images/logo/Esteghlal.png'),
+  Saipa: require('../assets/images/logo/Saipa.png'),
+  SanatNaft: require('../assets/images/logo/SanatNaftAbadan.png'),
+  Shahin: require('../assets/images/logo/Shahin.png'),
+  Tractor: require('../assets/images/logo/Tractor.png'),
+  Zobahan: require('../assets/images/logo/Zobahan.png'),
+  Parsjam: require('../assets/images/logo/Pars.png'),
+  Padide: require('../assets/images/logo/Padide.png'),
+  NaftMIS: require('../assets/images/logo/NaftMIS.png'),
+  Machine: require('../assets/images/logo/Machine.png'),
+  Foolad: require('../assets/images/logo/Foolad.png'),
 };
 export default club => logos[club];
